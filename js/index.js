0003336b53b556e1e8700e92fe2b1a5754b1e07a
booba302 <booba302@gmail.com>
2023-01-18 16:28:24 -0300

    segunda pre entrega

--- js/index.js
+++ js/index.js
@@ -1,28 +1,29 @@
 alert('Bienvenido al simulador de carrito de compra')
-
-let type = prompt('Ingrese la opción correspondiente: \n \n 1- Persona natural (IVA 14%). \n 2- Persona jurídica (IVA 16%). \n 3- Salir del programa ')
-type = parseInt(type)
+alert('Dependiendo si eres una persona o una empresa, el IVA será diferente.')
+let type = parseInt(prompt('Ingrese la opción correspondiente: \n \n 1- Persona (IVA 14%). \n 2- Empresa (IVA 16%). \n 3- Salir del programa '))
 
 const types = [1, 2, 3]
 
 while (!types.includes(type)) {
 
     alert('Ha seleccionado una opción incorrecta. Por favor intentelo de nuevo')
-    type = prompt('Ingrese la opción correspondiente: \n \n 1- Persona natural (IVA 14%). \n 2- Persona jurídica (IVA 16%). \n 3- Salir del programa ')
-    type = parseInt(type)
+    type = parseInt(prompt('Ingrese la opción correspondiente: \n \n 1- Persona natural (IVA 14%). \n 2- Persona jurídica (IVA 16%). \n 3- Salir del programa '))
 }
 
 let imp
 
-const menu = (type) => {
+menu(type)
+
+function menu(type) {
+
     switch (type) {
         case 1:
             imp = 0.14
-            calc(imp)
+            productList()
             break
         case 2:
             imp = 0.16
-            calc(imp)
+            productList()
             break
         case 3:
             alert('Saliendo del sistema')
@@ -30,31 +31,13 @@ const menu = (type) => {
     }
 }
 
-menu(type)
+function productList() {
 
-function calc(imp) {
-
-    let subTotal = 0
-    let iva = 0
-    let total = 0
     let products = []
 
-    let name = prompt('Ingrese el nombre del producto')
-    let price = parseInt(prompt('Ingrese el precio del producto'))
-
-    while (isNaN(price)) {
-
-        alert('No se ha ingresado un valor numérico. Por favor itentelo de nuevo.')
-        price = parseInt(prompt('Ingrese el precio del producto'))
-
-    }
-
-    products.push(newProducts(name, price))
-
-    //subTotal = subTotal + product
+    addProduct(products)
 
     let newProduct = prompt('¿Desea agregar otro producto?\n\n- Si. \n- No. \n')
-
     const options = ['si', 'no', 'Si', 'No']
 
     while (!options.includes(newProduct)) {
@@ -63,105 +46,127 @@ function calc(imp) {
         newProduct = prompt('¿Desea agregar otro producto?\n\n- Si. \n- No. \n')
     }
 
-    while (newProduct == 'si' || newProduct == 'Si') {
+    const add = ['Si','si']
 
-        name = prompt('Ingrese el nombre del producto')
-        price = parseInt(prompt('Ingrese el precio del producto'))
+    while (add.includes(newProduct)) {
 
-        while (isNaN(price)) {
-
-            alert('No ha agregado un valor numérico, intente de nuevo.')
-            price = parseInt(prompt('Ingrese el precio del producto'))
-        }
-
-        products.push(newProducts(name, price))
-
+        addProduct(products)
         newProduct = prompt('¿Desea agregar otro producto?\n\n- Si. \n- No. \n')
     }
 
-    let nro = 1
-    let message = summary(products, nro)
+    let payment = (parseInt(prompt('El resumen de su compra es: \n' + summary(products) + '\n0) Continuar' + '\n\nIngrese 0 para continuar con el pago o si desea eliminar un producto ingrese el número correspondiente')))
 
-    let pay = parseInt(prompt('El resumen de su compra es: \n' + message + '\n0) Continuar' + '\n\nIngrese 0 para continuar con el pago o si desea eliminar un producto ingrese el número correspondiente'))
-
-    while (isNaN(pay)) {
+    while (isNaN(payment) || payment < 0 || payment > products.length) {
 
         alert('No se ha seleccionado una opción correcta.\nPor favor intente nuevamente intente nuevamente')
-        pay = parseInt(prompt('El resumen de su compra es: \n' + message + '\n0) Continuar' + '\n\nIngrese 0 para continuar con el pago o si desea eliminar un producto ingrese el número correspondiente'))
+        payment = (parseInt(prompt('El resumen de su compra es: \n' + summary(products) + '\n0) Continuar' + '\n\nIngrese 0 para continuar con el pago o si desea eliminar un producto ingrese el número correspondiente')))
     }
 
-    while (pay >= 1 && pay <= 1 + products.length) {
+    while (payment > 0) {
 
-        if (products.length = 0) {
+        delProduct(products, payment)
 
-            alert('No tienes productos agregados a tu carrito')
-            menu(3)
+        if (products.length == 0) {
+
+            payment = null
+            alert('No tienes productos en tu carrito.')
 
         } else {
 
-            del = pay - 1
-            deleteProducts(products, del)
-            message = summary(products, nro)
-            pay = parseInt(prompt('El resumen de su compra es: \n' + message + '\n0) Continuar' + '\n\nIngrese 0 para continuar con el pago o si desea eliminar un producto ingrese el número correspondiente'))
+            payment = (parseInt(prompt('El resumen de su compra es: \n' + summary(products) + '\n0) Continuar' + '\n\nIngrese 0 para continuar con el pago o si desea eliminar un producto ingrese el número correspondiente')))
+
         }
     }
 
-    for (i = 0; i < products.length; i++) {
+    if (payment == null) {
 
-        subTotal = subTotal + products[i].price
+        menu(3)
+
+    } else {
+
+        calc(products, imp)
+        menu(3)
     }
-
-    iva = subTotal * imp
-    total = subTotal + iva
-
-    let cuotas = prompt('¿En cuantas cuotas pagará?')
-    cuotas = parseInt(cuotas)
-
-    while (isNaN(cuotas) || cuotas <= 0) {
-
-        alert('No se ha seleccionado una cantidad de cuotas correcta.\nIntente nuevamente')
-        cuotas = prompt('¿En cuantas cuotas pagará?')
-        cuotas = parseInt(cuotas)
-    }
-
-    let cuota = total / cuotas
-
-    alert('El resumen de su compra es el suguiente: \n\nSubtotal: ' + subTotal + '\nIva: ' + iva.toFixed(2) + '\nTotal: ' + total + '\nNro de cuotas: ' + cuotas + '\nCuotas: ' + cuota.toFixed(2))
-
 }
 
-function newProducts(name, price) {
+function addProduct(products) {
 
     class Product {
+
         constructor(name, price) {
             this.name = name
             this.price = price
         }
         info() {
+
             console.log('El nombre del producto es: ' + name + ' y su precio es: ' + price);
         }
     }
 
-    return new Product(name, price)
+    let name = prompt('Ingrese el nombre del producto')
+    let price = parseInt(prompt('Ingrese el precio del producto'))
+
+    while (isNaN(price) || price < 0) {
+
+        alert('No se ha ingresado un valor numérico. Por favor itentelo de nuevo.')
+        price = parseInt(prompt('Ingrese el precio del producto'))
+    }
+
+    products.push(new Product(name, price))
+
+    return products
 }
 
-function summary(products, nro) {
+function delProduct(products, payment) {
+
+    let del = payment - 1
+
+    alert('El siguiente producto fue eliminado: ' + products[del].name + ' ' + products[del].price)
+
+    products = products.splice(del, 1)
+
+    return products
+}
+
+function summary(products) {
 
     let message = ''
+    let nro = 1
+    let resumen = ''
 
-    for (let i = 0; i < products.length; i++) {
+    products.forEach(product => {
 
-        let resumen = '\n' + nro + ') ' + products[i].name + ' ' + products[i].price + '$'
+        resumen = '\n' + nro + ') ' + product.name + ' ' + product.price + '$'
         message = message + resumen
-
         nro++
-    }
+    })
 
     return message
 }
 
-function deleteProducts(products, del) {
+function calc(products, imp) {
 
-    products = products.splice(del, 1)
-    return products
+    let subTotal = 0
+    let iva = 0
+    let total = 0
+
+    products.forEach(product => {
+
+        subTotal = subTotal + product.price
+    })
+
+    iva = subTotal * imp
+    total = subTotal + iva
+
+    let cuotas = parseInt(prompt('¿En cuantas cuotas pagará?'))
+
+    while (isNaN(cuotas) || cuotas <= 0) {
+
+        alert('No se ha seleccionado una cantidad de cuotas correcta.\nIntente nuevamente')
+        cuotas = parseInt(prompt('¿En cuantas cuotas pagará?'))
+    }
+
+    let cuota = total / cuotas
+
+    alert('El resumen de su compra es el suguiente: \n\nSubtotal: ' + subTotal + '\nIva: ' + iva.toFixed(2) + '\nTotal: ' + total + '\nNro de cuotas: ' + cuotas + '\nCuotas: ' + cuota.toFixed(2))
 }
