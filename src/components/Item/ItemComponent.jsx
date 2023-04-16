323824d278919ef46062a9e0264875caa375b16a
Franklin Sanchez <booba302@gmail.com>
2023-04-16 19:20:04 -0400

    segunda pre entrega

--- src/components/Item/ItemComponent.jsx
+++ src/components/Item/ItemComponent.jsx
@@ -15,12 +15,13 @@ const ItemComponent = (props) => {
     <div className="card shadow-sm m-4">
       <div className="card-header">
         <h5>{showShortValue(title, 35)}</h5>
+        <NavLink to={`/products/` + id}>
+          <button className="btn btn btn-outline-success btn-sm">+info</button>
+        </NavLink>
       </div>
       <div className="card-body">
         <p>{showShortValue(description, 40)}</p>
-        <NavLink to={`/products/` + id}>
-          <img width={400} height={300} src={image} alt="" />
-        </NavLink>
+        <img width={400} height={300} src={image} alt="" />
         <p>{price}$</p>
         <button className="btn btn-success">Agregar</button>
       </div>
